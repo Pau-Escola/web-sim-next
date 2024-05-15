e64285ddac04881da3c25d9b84d51998678fa6fe
Pau Escola <pau.escola@sky.uk>
2024-05-15 18:51:40 +0100

    Com que el server de arsys no ens deixa executar scripts haurem de fer el que ens temiem fer les pagines per triplicat amb el corresponent idioma

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 // next.config.mjs
-const { i18n } = require('./next-i18next.config.js');
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
