cb9039ca09be989e7d834badba9ab6457348f6bf
Pau Escola <pau.escola@sky.uk>
2024-05-15 18:53:49 +0100

    ups

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 // next.config.mjs
-
+const { i18n } = require('./next-i18next.config.js');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
