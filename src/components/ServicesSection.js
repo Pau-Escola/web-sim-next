7488cae94c1c98a2c35b9b6a9751efc31da9edf9
Pau Escola <pau.escola@sky.uk>
2024-06-01 15:34:06 +0100

    Sembla que ara ja hem arreglat el problema amb els services

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -13,9 +13,9 @@ function ServicesSection() {
 
     return (
         <div className="bg-primary py-8">
-            <div className="max-w-6xl mx-auto px-4">
+            <div className="max-w-6xl mx-auto px-1 md:px-4">
                 <h2 className="text-3xl md:text-4xl font-bold text-white text-center mb-6">{t('Our services')}</h2>
-                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 md:gap-6 gap-1">
                     {[
                         { icon: rentIcon, title: 'Rent', text: 'Rent Text' },
                         { icon: buyIcon, title: 'Buy', text: 'Buy Text' },
@@ -25,8 +25,8 @@ function ServicesSection() {
                         { icon: tradingIcon, title: 'Trading', text: 'Trading Text' }
                     ].map((service, index) => (
                         <div key={index} className="group relative md:border-4 bg-white sm:w-full text-primary border-primary shadow-sm rounded-lg overflow-hidden">
-                            <Image src={service.icon} alt={`${service.title}`} className="w-64 h-64 mx-auto pt-5" />
-                            <h3 className="text-2xl text-center mt-2 font-semibold">{t(service.title)}</h3>
+                            <Image src={service.icon} alt={`${service.title}`} className="sm:w-32 md:w-64 sm:h-32 md:h-64 mx-auto pt-5" />
+                            <h3 className="sm:text-xl md:text-2xl text-center mt-2 font-semibold">{t(service.title)}</h3>
                             <div className="absolute inset-0 bg-primary bg-opacity-95 scale-0 group-hover:scale-100 transition-transform duration-300 ease-in-out flex justify-center items-center opacity-0 group-hover:opacity-100">
                                 <p className="text-sm sm:text-base md:text-lg p-4 text-white"><Trans i18nKey={`${service.text}`} /></p>
                             </div>
